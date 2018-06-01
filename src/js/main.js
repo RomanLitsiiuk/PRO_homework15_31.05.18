cbfb8b6e137c19a635df8c2f7c736f3c87140788
Roman Litsiiuk <alexandrovich2015@gmail.com>
2018-06-01 18:45:33 +0300

    Accordion constructor was created

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,21 @@
-var title = document.getElementsByClassName('accordion-title');
-var cont = document.getElementsByClassName('accordion-cont');
+var Accordion = function (rootElement, className) {
+  this.rootElement = rootElement;
+  this.className = className;
+  this.elementsList = rootElement.querySelectorAll('.accordion-title');
+  this.changeActiveClass(this.elementsList, this.className);
+};
 
-/*for (var i = 0; i < title.length; i++) {
-  title[i].addEventListener('click', function () {
-    console.log(this.classList);
-    if(!(this.classList.contains('active'))) {
-      for (var j = 0; j < title.length; j++) {
-        title[j].classList.remove('active');
+Accordion.prototype.changeActiveClass = function (elementsList, activeClass) {
+  elementsList.forEach(function (element) {
+    element.addEventListener('click', function () {
+      if (!(element.classList.contains(activeClass))) {
+        elementsList.forEach(function (element) {
+          element.classList.remove(activeClass);
+        });
+        this.classList.add(activeClass);
       }
-      this.classList.add('active');
-    }
-  })
-}*/
+    })
+  });
+};
 
-for (var i = 0; i < title.length; i++) {
-  var currentTitle = title[i];
-  currentTitle.addEventListener('click', function () {
-    var classes = this.getAttribute('class');
-    var newClasses = '';
-    var classesArr = classes.split(' ');
-    var newClassesArr = classes.split(' ');
-    for (var j = 0; j <classesArr.length; j++) {
-      if (classesArr[j] === 'active') {
-        classesArr.splice([j], 1);
-      }
-    }
-    if (classesArr.length === newClassesArr.length) {
-      classesArr.push('active');
-      newClasses = classesArr.join(' ');
-    } else {
-      newClasses = classesArr.join(' ');
-    }
-    for (var l = 0; l < title.length; l++) {
-      oldClasses = title[l].getAttribute('class');
-      oldClassesArr = oldClasses.split(' ');
-      for (var k = 0; k < oldClassesArr.length; k++) {
-        if (oldClassesArr[k] === 'active') {
-          oldClassesArr.splice([k], 1);
-        }
-        oldClasses = oldClassesArr.join(' ');
-        title[l].setAttribute('class', oldClasses);
-      }
-    }
-    this.setAttribute('class', newClasses);
-  })
-}
+var accordion1 = new Accordion(document.getElementById('accordion'), 'active');
